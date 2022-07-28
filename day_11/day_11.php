ab296df6113e9401b448e6bf292de1244e20cd2a
Raül Navarro <raulnavarro@smartcommerce.co>
2022-07-28 13:17:38 +0200

    day_11 part 2

--- day_11/day_11.php
+++ day_11/day_11.php
@@ -8,58 +8,62 @@ foreach ($input as $i => $fila)
 		if($j < 10) $octopus_map[$i][$j] = $element;
 
 $count = 0;
-for ($k = 0; $k < 100; ++$k) {
+$k = 0;
+do {
+	$all_flashed = 0;
 	$flasheable = array_fill(0, 10, array_fill(0, 10, true));
 	foreach ($octopus_map as $i => $fila) {
 		foreach ($fila as $j => $octopus) {
 			if (!$flasheable[$i][$j])
 				continue;
 			else
-				suma($i, $j, $octopus_map, $flasheable, $count);
+				suma($i, $j, $octopus_map, $flasheable, $count, $all_flashed);
 		}
 	}
-}
+	$k++;
+} while($all_flashed < 100);
 
-echo $count . "\n";
+echo $k . "\n";
 
-function sumaveins($i, $j, &$octopus_map, &$flasheable, &$count) {
+function sumaveins($i, $j, &$octopus_map, &$flasheable, &$count, &$all_flashed) {
 	if (isvalid($i-1, $j-1) && $flasheable[$i-1][$j-1])
-		suma($i-1, $j-1, $octopus_map, $flasheable, $count);
+		suma($i-1, $j-1, $octopus_map, $flasheable, $count, $all_flashed);
 
 	if (isvalid($i-1, $j) && $flasheable[$i-1][$j])
-		suma($i-1, $j, $octopus_map, $flasheable, $count);
+		suma($i-1, $j, $octopus_map, $flasheable, $count, $all_flashed);
 
 	if (isvalid($i-1, $j+1) && $flasheable[$i-1][$j+1])
-		suma($i-1, $j+1, $octopus_map, $flasheable, $count);
+		suma($i-1, $j+1, $octopus_map, $flasheable, $count, $all_flashed);
 
 	if (isvalid($i, $j-1) && $flasheable[$i][$j-1])
-		suma($i, $j-1, $octopus_map, $flasheable, $count);
+		suma($i, $j-1, $octopus_map, $flasheable, $count, $all_flashed);
 
 	if (isvalid($i, $j+1) && $flasheable[$i][$j+1])
-		suma($i, $j+1, $octopus_map, $flasheable, $count);
+		suma($i, $j+1, $octopus_map, $flasheable, $count, $all_flashed);
 
 	if (isvalid($i+1, $j-1) && $flasheable[$i+1][$j-1])
-		suma($i+1, $j-1, $octopus_map, $flasheable, $count);
+		suma($i+1, $j-1, $octopus_map, $flasheable, $count, $all_flashed);
 
 	if (isvalid($i+1, $j) && $flasheable[$i+1][$j])
-		suma($i+1, $j, $octopus_map, $flasheable, $count);
+		suma($i+1, $j, $octopus_map, $flasheable, $count, $all_flashed);
 
 	if (isvalid($i+1, $j+1) && $flasheable[$i+1][$j+1])
-		suma($i+1, $j+1, $octopus_map, $flasheable, $count);
+		suma($i+1, $j+1, $octopus_map, $flasheable, $count, $all_flashed);
 }
 
-function suma($i, $j, &$octopus_map, &$flasheable, &$count) {
+function suma($i, $j, &$octopus_map, &$flasheable, &$count, &$all_flashed) {
 	$octopus_map[$i][$j]++;
 	if($octopus_map[$i][$j] > 9) {
-		countFlash($i, $j, $count, $flasheable, $octopus_map);
-		sumaveins($i, $j, $octopus_map, $flasheable, $count);
+		countFlash($i, $j, $count, $flasheable, $octopus_map, $all_flashed);
+		sumaveins($i, $j, $octopus_map, $flasheable, $count, $all_flashed);
 	}
 }
 
-function countFlash($i, $j, &$count, &$flasheable, &$octopus_map) {
+function countFlash($i, $j, &$count, &$flasheable, &$octopus_map, &$all_flashed) {
 	if ($flasheable[$i][$j]) {
 		$flasheable[$i][$j] = false;
 		++$count;
+		++$all_flashed;
 		$octopus_map[$i][$j] = 0;
 	}
 }
